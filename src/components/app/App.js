3ea5166cbec48232b0fe85e8873f7b296ef6305f
den <viewsonic352@gmail.com>
2024-10-28 22:00:18 +0200

    update weather, hourly, days, app media-request

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -4,51 +4,45 @@ import DaysForecast from "../daysForecast/DaysForecast";
 import HourlyForecast from "../hourlyForecast/HourlyForecast";
 import { useState, useEffect } from "react";
 
-
 const App = () => {
+  const [city, setCity] = useState("Kyiv");
+  const [data, setData] = useState({});
 
-    const [city, setCity] = useState('Kyiv')
-    const [data, setData] = useState({})
+  const getResource = async (url) => {
+    let res = await fetch(url);
 
-    const getResource = async(url) => {
-        let res = await fetch(url);
-
-        if (!res.ok) {
-            throw new Error('Could not fetch ${url}, status: ${res.status}');
-        }
-
-        const data = res.json();
-        return data
+    if (!res.ok) {
+      throw new Error("Could not fetch ${url}, status: ${res.status}");
     }
 
-    
-    // useEffect(() => {
-    //     getResource(`http://api.weatherapi.com/v1/current.json?key=d4482eb276b541a8a1b161131240804&q=${city}&aqi=yes`).then(data => setData(data))
-    // }, [])
+    const data = res.json();
+    return data;
+  };
 
-    useEffect(() => {
-        getResource(`https://api.weatherapi.com/v1/current.json?key=d4482eb276b541a8a1b161131240804&q=${city}&days=5&aqi=yes&alerts=no`).then(data => setData(data))
-    }, [city])
+  useEffect(() => {
+    getResource(
+      `https://api.weatherapi.com/v1/current.json?key=d4482eb276b541a8a1b161131240804&q=${city}&days=5&aqi=yes&alerts=no`
+    ).then((data) => setData(data));
+  }, [city]);
 
-    
-    const { location: { region, name } = {}, 
-            current: {temp_c } = {},
-            forecast: { forecastday } = {},
-        } = data 
+  const {
+    location: { region, name } = {},
+    current: { temp_c } = {},
+    forecast: { forecastday } = {},
+  } = data;
 
-
-    return (
-        <div className="app">
-            <AppHeader setCity={setCity}/>
-            <main>
-                <WeatherIndexes city={city} getResource={getResource} data={data} />
-                <div className="timetable__content">
-                    <DaysForecast  city={city} getResource={getResource} data={data}/>
-                    <HourlyForecast city={city} getResource={getResource} />
-                </div>
-            </main>
+  return (
+    <div className="app">
+      <AppHeader setCity={setCity} />
+      <main>
+        <WeatherIndexes city={city} getResource={getResource} data={data} />
+        <div className="timetable__content">
+          <DaysForecast city={city} getResource={getResource} data={data} />
+          <HourlyForecast city={city} getResource={getResource} />
         </div>
-    )
-}
+      </main>
+    </div>
+  );
+};
 
 export default App;
--- src/components/weatherIndexes/WeatherIndexes.js
+++ src/components/weatherIndexes/WeatherIndexes.js
@@ -1,151 +1,173 @@
-import React, { useEffect, useState } from 'react';
-import './weatherIndexes.scss';
+import React, { useEffect, useState } from "react";
+import "./weatherIndexes.scss";
 
-import sunriseLogo from '../../resources/img/Sun/sunrise-white.png'
-import sunsetLogo from '../../resources/img/Sun/sunset-white.png'
+import sunriseLogo from "../../resources/img/Sun/sunrise-white.png";
+import sunsetLogo from "../../resources/img/Sun/sunset-white.png";
 // import SunnyLogo from '../../resources/img/Sun/clearMax.png'
-import humidityLogo from '../../resources/img/Sun/humidity.png'
-import windLogo from '../../resources/img/Sun/wind.png'
-import pressureLogo from '../../resources/img/Sun/pressure-white.png'
-import uvLogo from '../../resources/img/Sun/uv-white.png'
-
+import humidityLogo from "../../resources/img/Sun/humidity.png";
+import windLogo from "../../resources/img/Sun/wind.png";
+import pressureLogo from "../../resources/img/Sun/pressure-white.png";
+import uvLogo from "../../resources/img/Sun/uv-white.png";
 
 const WeatherIndexes = ({ city, getResource }) => {
-    const [weatherData, setWeatherData] = useState({
-        name: null,
-        time: null,
-        day: null,
-        selsius: null,
-        feelsLike: null,
-        sunriseAm: null,
-        sunsetAm: null,
-        sunnyLogo: null,
-        condition: null,
-        conditionText: null,
-        humidityPercent: null,
-        pressureIndexes: null,
-        windIndexes: null,
-        uvIndexes: null
-    });
+  const [weatherData, setWeatherData] = useState({
+    name: null,
+    time: null,
+    day: null,
+    selsius: null,
+    feelsLike: null,
+    sunriseAm: null,
+    sunsetAm: null,
+    sunnyLogo: null,
+    condition: null,
+    conditionText: null,
+    humidityPercent: null,
+    pressureIndexes: null,
+    windIndexes: null,
+    uvIndexes: null,
+  });
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const res = await getResource(`https://api.weatherapi.com/v1/forecast.json?key=d4482eb276b541a8a1b161131240804&q=${city}&days=5&aqi=yes&alerts=no`);
-                setWeatherData({
-                    name: res.location.name,
-                    time: res.location.localtime.slice(11),
-                    day: res.location.localtime.slice(0, 11),
-                    selsius: res.current.temp_c,
-                    feelsLike: res.current.feelslike_c,
-                    sunriseAm: res.forecast.forecastday[0].astro.sunrise,
-                    sunsetAm: res.forecast.forecastday[0].astro.sunset,
-                    sunnyLogo: res.current.condition?.icon || null,
-                    condition: res.forecast.forecastday[0].hour[0].condition.icon,
-                    conditionText: res.forecast.forecastday[0].hour[0].condition.text,
-                    humidityPercent: res.current.humidity,
-                    pressureIndexes: res.current.pressure_mb,
-                    windIndexes: res.current.wind_kph,
-                    uvIndexes: res.current.uv
-                });
-            } catch (error) {
-                console.error('Error fetching weather data:', error);
-            }
-        };
-    
-        fetchData();
-    
-        const intervalId = setInterval(fetchData, 1 * 60 * 1000);
-    
-        return () => clearInterval(intervalId);
-    }, [city, getResource]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await getResource(
+          `https://api.weatherapi.com/v1/forecast.json?key=d4482eb276b541a8a1b161131240804&q=${city}&days=5&aqi=yes&alerts=no`
+        );
+        setWeatherData({
+          name: res.location.name,
+          time: res.location.localtime.slice(11),
+          day: res.location.localtime.slice(0, 11),
+          selsius: res.current.temp_c,
+          feelsLike: res.current.feelslike_c,
+          sunriseAm: res.forecast.forecastday[0].astro.sunrise,
+          sunsetAm: res.forecast.forecastday[0].astro.sunset,
+          sunnyLogo: res.current.condition?.icon || null,
+          condition: res.forecast.forecastday[0].hour[0].condition.icon,
+          conditionText: res.forecast.forecastday[0].hour[0].condition.text,
+          humidityPercent: res.current.humidity,
+          pressureIndexes: res.current.pressure_mb,
+          windIndexes: res.current.wind_kph,
+          uvIndexes: res.current.uv,
+        });
+      } catch (error) {
+        console.error("Error fetching weather data:", error);
+      }
+    };
 
-    const { name, time, day, selsius, feelsLike, sunriseAm, sunsetAm, sunnyLogo,
-        conditionText, humidityPercent, pressureIndexes, windIndexes, uvIndexes } = weatherData;
+    fetchData();
 
-    return (
-        <div className="weather">
-            <div className="weather__block">
-                <div className="weather__info">
-                    <p className="weather__name">{name}</p>
-                    <p className="weather__time">{time}</p>
-                    <p className="weather__day">{day}</p>
-                </div>
-            </div>
+    const intervalId = setInterval(fetchData, 1 * 60 * 1000);
 
-            <div className="weather__static">
-                <div className='weather__firstblock'>
-                    <div>
-                        <p className="weather__firstblock-celsius">{selsius}°C</p>
-                        <div className='weather__firstblock-block'>
-                            <p className="weather__firstblock-like">Feels like:</p>
-                            <p className="weather__firstblock-likecels">{feelsLike}°C</p>
-                        </div>
-                    </div>
-                    <div className='weather__firstblock-sunriseblock'>
-                        <div className='weather__firstblock-sunblock'>
-                            <div className='weather__firstblock-logo'>
-                                <img src={sunriseLogo} alt="sunrise" />
-                            </div>
+    return () => clearInterval(intervalId);
+  }, [city, getResource]);
 
-                            <p className="weather__firstblock-sunrise">Sunrise <br /> {sunriseAm}</p>
-                        </div>
+  const {
+    name,
+    time,
+    day,
+    selsius,
+    feelsLike,
+    sunriseAm,
+    sunsetAm,
+    sunnyLogo,
+    conditionText,
+    humidityPercent,
+    pressureIndexes,
+    windIndexes,
+    uvIndexes,
+  } = weatherData;
 
-                        <div className='weather__firstblock-sunblock'>
-                            <div className='weather__firstblock-logo'>
-                                <img src={sunsetLogo} alt="sunrise" />
-                            </div>
-                            <p className="weather__firstblock-sunset">Sunset <br /> {sunsetAm}</p>
-                        </div>
-
-                    </div>
-                </div>
-
-                <div className='weather__secondblock'>
-                    <div className='weather__secondblock-logo'>
-                        <img src={sunnyLogo} alt="sunny" />
-                    </div>
-
-                    <p className="weather__secondblock-sunny">{conditionText}</p>
-                </div>
-
-                <div className='weather__thirdblock'>
-
-                    <div className='weather__thirdblock-humidity'>
-                        <div className='weather__thirdblock-humlogo'>
-                            <img src={humidityLogo} alt="photo" />
-                        </div>
-                        <p className="weather__thirdblock-huminterest">{humidityPercent}% </p>
-                        <p className="weather__thirdblock-humtext">Humidity</p>
-                    </div>
-
-                    <div className='weather__thirdblock-wind'>
-                        <div className='weather__thirdblock-windlogo'>
-                            <img src={windLogo} alt="photo" />
-                        </div>
-                        <p className="weather__thirdblock-windinterest">{windIndexes}km/h</p>
-                        <p className="weather__thirdblock-windtext">Wind speed</p>
-                    </div>
-
-                    <div className='weather__thirdblock-pressblock'>
-                        <div className='weather__thirdblock-presslogo'>
-                            <img src={pressureLogo} alt="photo" />
-                        </div>
-                        <p className="weather__thirdblock-pressinterest">{pressureIndexes}hPa</p>
-                        <p className="weather__thirdblock-presstext">Pressure</p>
-                    </div>
-
-                    <div className='weather__thirdblock-uv'>
-                        <div className='weather__thirdblock-uvlogo'>
-                            <img src={uvLogo} alt="photo" />
-                        </div>
-                        <p className="weather__thirdblock-uvinterest">{uvIndexes}</p>
-                        <p className="weather__thirdblock-uvtext">UV</p>
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="weather">
+      <div className="weather__block">
+        <div className="weather__info">
+          <p className="weather__name">{name}</p>
+          <p className="weather__time">{time}</p>
+          <p className="weather__day">{day}</p>
         </div>
-    );
+      </div>
+
+      <div className="weather__static">
+        <div className="weather__firstblock">
+          <div>
+            <p className="weather__firstblock-celsius">{selsius}°C</p>
+            <div className="weather__firstblock-block">
+              <p className="weather__firstblock-like">Feels like:</p>
+              <p className="weather__firstblock-likecels">{feelsLike}°C</p>
+            </div>
+          </div>
+          <div className="weather__firstblock-sunriseblock">
+            <div className="weather__firstblock-sunblock">
+              <div className="weather__firstblock-logo">
+                <img src={sunriseLogo} alt="sunrise" />
+              </div>
+
+              <p className="weather__firstblock-sunrise">
+                Sunrise <br /> {sunriseAm}
+              </p>
+            </div>
+
+            <div className="weather__firstblock-sunblock">
+              <div className="weather__firstblock-logo">
+                <img src={sunsetLogo} alt="sunrise" />
+              </div>
+              <p className="weather__firstblock-sunset">
+                Sunset <br /> {sunsetAm}
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="weather__secondblock">
+          <div className="weather__secondblock-logo">
+            <img src={sunnyLogo} alt="sunny" />
+          </div>
+
+          <p className="weather__secondblock-sunny">{conditionText}</p>
+        </div>
+
+        <div className="weather__thirdblock">
+          <div className="weather__thirdblock-humidity">
+            <div className="weather__thirdblock-humlogo">
+              <img src={humidityLogo} alt="photo" />
+            </div>
+            <p className="weather__thirdblock-huminterest">
+              {humidityPercent}%{" "}
+            </p>
+            <p className="weather__thirdblock-humtext">Humidity</p>
+          </div>
+
+          <div className="weather__thirdblock-wind">
+            <div className="weather__thirdblock-windlogo">
+              <img src={windLogo} alt="photo" />
+            </div>
+            <p className="weather__thirdblock-windinterest">
+              {windIndexes}km/h
+            </p>
+            <p className="weather__thirdblock-windtext">Wind speed</p>
+          </div>
+
+          <div className="weather__thirdblock-pressblock">
+            <div className="weather__thirdblock-presslogo">
+              <img src={pressureLogo} alt="photo" />
+            </div>
+            <p className="weather__thirdblock-pressinterest">
+              {pressureIndexes}hPa
+            </p>
+            <p className="weather__thirdblock-presstext">Pressure</p>
+          </div>
+
+          <div className="weather__thirdblock-uv">
+            <div className="weather__thirdblock-uvlogo">
+              <img src={uvLogo} alt="photo" />
+            </div>
+            <p className="weather__thirdblock-uvinterest">{uvIndexes}</p>
+            <p className="weather__thirdblock-uvtext">UV</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default WeatherIndexes;
